470b602400f0f77b2b276db293ba759c020388ae
BhavinRaichura <bhavinraichura28@gmail.com>
2022-01-22 02:55:04 +0530

    commit

--- application/route.py
+++ application/route.py
@@ -1,6 +1,21 @@
 from application import app
 from flask import Flask, request, url_for, render_template, redirect
+from twilio.rest import Client 
+from application.twilio_auth import collection as tauth
+
+
+
+client = Client(tauth['account_sid'], tauth['auth_token']) 
 
 @app.route("/")
 def home():
     return "<h1>hello world</h1>"
+
+@app.route("/<string:msg>")
+def msg(msg):
+    return client.messages.create( 
+                              from_='whatsapp:+14155238886',  
+                              body=msg,      
+                              to='whatsapp:+918085129146' 
+                          ) 
+     
