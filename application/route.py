fff309b22089970178d957feba065119201a2b5b
BhavinRaichura <bhavinraichura28@gmail.com>
2022-01-25 17:06:17 +0530

    commit

--- application/route.py
+++ application/route.py
@@ -1,16 +1,14 @@
 from application import app
 from flask import Flask, request, url_for, render_template, redirect,session,request
+from application import db
 from twilio.rest import Client 
 from application.twilio_auth import collection as tauth
 from application.tpo import tpo_data 
-
 
 from twilio.twiml.messaging_response import MessagingResponse
 import time
 
 app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
-
-
 
 session={'user':'student'}
 
@@ -20,7 +18,18 @@ client = Client(tauth['account_sid'], tauth['auth_token'])
 # jupyter notebook data ------------------------------------------------------1
 
 
+# time table
+oe="Open elective\nhttps://meet.google.com/rjz-pghg-jrb"
+b="Biomaterials\nhttps://meet.google.com/rmb-kayq-wrf"
+m="Microelectronics & ic\nhttps://meet.google.com/acf-tnhp-fry"
+t="Telemedicine\nhttps://meet.google.com/keo-qqht-tqd"
+d="BDMS\nhttps://rb.gy/wlydv3"
 
+timeSlot = ["09:30","10:30","11:30","12:30"]
+time_table ={"mon":[b,oe,m,t],"tue":[b,t,m,d],"wed":[b,oe,d,m],"thu":[d,b,m,t],"fri":[d,oe,t,m],"sat":["-noClass-","-noClass-","-noClass-","-noClass-"],"sun":["-noClass-","-noClass-","-noClass-","-noClass-"]}
+days =["mon","tue","wed","thu","fri","sat","sun"]
+
+#----------------------------------------------------------------------------------------------------------------------------------------------------
 @app.route("/")
 def home():
     return "<h1>BME Bot</h1>"
@@ -34,6 +43,8 @@ def msg(mailscript):
                               to='whatsapp:+918085129146' 
                           ) 
 
+#--------------------------------------------------------------------------------------
+# bot
 
 @app.route('/bot',methods=['GET','POST'])
 def bot():
@@ -43,12 +54,8 @@ def bot():
     bot_resp=MessagingResponse()
     msg=bot_resp.message()
 
+    mob=str(user_mobile)[12:]
     user_name=""
-    mob=int(str(user_mobile)[12:])
-    if mob in mobile_collection:
-        user_name = mobile_collection[mob]
-    print("\n{}: {}\n{} ".format(user_name,str(user_msg),mob))
-
     reply=""
 
     if "time table" in user_msg or "classes" in user_msg:
@@ -58,7 +65,6 @@ def bot():
             reply ="Data not found"
             msg.body(reply)
             return str(bot_resp)
-
         
         for i in days:
             if i in user_msg:
@@ -78,31 +84,22 @@ def bot():
 
 
     elif 'classroom' in user_msg or 'class room' in user_msg or 'google class' in user_msg:
-        for i in range(len(google_classroom)):
-            reply += f"\n{google_classroom[i][0]}: {google_classroom[i][1]}"
+        reply=db.class_room_details()
         msg.body(reply)
         return str(bot_resp)
 
     elif "tpo" in user_msg or "internship" in user_msg or "placement" in user_msg or "company" in user_msg or "companies" in user_msg:
-        tpo_data.sort()
-        if "deadline" in user_msg or "recent" in user_msg or "recently" in user_msg:
-            reply += f"{tpo_data[0][2]}\n\nDeadline: {tpo_data[0][0]}\nLink: {tpo_data[0][1]}"
-        else:
-            for i in range(len(tpo_data)):
-                reply += f"{tpo_data[i][2]}\n\nDeadline: {tpo_data[i][0]}\nLink: {tpo_data[i][1]}\n---------/////////////////--------\n"
+        reply = db.interns_details()
         msg.body(reply)
         return str(bot_resp)
 
-    elif "event" in user_msg or "upcoming event" in user_msg or "clubs" in user_msg or "tcp" in user_msg or "update" in user_msg:
-        upCommingEvents.sort()
-        
-        for i in range(len(upCommingEvents)):
-            reply += f"{upCommingEvents[i][2]}\n\nDeadline: {upCommingEvents[i][0]}\nLink: {upCommingEvents[i][1]}\n---------/////////////////--------\n"
+    elif "event" in user_msg or "upcoming event" in user_msg or "clubs" in user_msg :
+        reply=db.events_details()
         msg.body(reply)
         return str(bot_resp)
 
-    elif "hello" in user_msg or "hi" in user_msg or "hey" in user_msg:
-        reply = f"Hello {user_name}!\nHow are you?"
+    elif "update" in user_msg or "announce" in user_msg:
+        reply = db.new_updates_details()
         msg.body(reply)
         return str(bot_resp)
 
@@ -111,9 +108,50 @@ def bot():
         msg.body(reply)
         return str(bot_resp)
 
+    elif "hello" in user_msg or "hi" in user_msg or "hey" in user_msg:
+        if db.db.users.find({"mobile_no":mob}):
+            valid_user = db.db.users.find_one({"mobile_no":mob})
+            user_name = valid_user['name']
+        reply = f"Hello {user_name}!\nHow are you?"
+        msg.body(reply)
+        return str(bot_resp)
 
     msg.body(f"Hello {user_name}!\nI am BME bot. I can help you in following operations:\n*time table* | *classes* | *events* | *updates* | *tpo* | *clg*")
     return str(bot_resp)
 
 
+#--------------------------------------------------------------------------------------
+# update database
+#
+@app.route("/newusersdb",methods =['POST','GET'])
+def newusersdb():
+    if request.method == "POST":
+        dataset={"name":request.form['std_name'],"roll_no":request.form['rollno'],"mobile_no":request.form['mobile'],"email":request.form['email'],"open_elective":request.form['oe'],"sem":6}
+        db.db.users.insert_one(dataset)
+        return "<h1>data successfully added</h1>"
+    return render_template('index.html',pagetitle="New User")
+
+@app.route("/tpodb",methods =['POST','GET'])
+def tpodb():
+    if request.method == "POST":
+        dataset={"deadline":request.form['deadline'],"link":request.form['link'],"subj":request.form['subj']}
+        db.db.tpo.insert_one(dataset)
+        return "<h1>data successfully added</h1>"
+    return render_template('index.html',pagetitle="TPO")
+
+@app.route("/eventsdb",methods =['POST','GET'])
+def eventsdb():
+    if request.method == "POST":
+        dataset={"deadline":request.form['deadline'],"link":request.form['link'],"subj":request.form['subj']}
+        db.db.events.insert_one(dataset)
+        return "<h1>data successfully added</h1>"
+    return render_template('index.html',pagetitle="Events")
+
+@app.route("/newupdatesdb",methods =['POST','GET'])
+def newUpdatesdb():
+    if request.method == "POST":
+        dataset={"subj":request.form['subj']}
+        db.db.class_update.insert_one(dataset)
+        return "<h1>data successfully added</h1>"
+    return render_template('index.html',pagetitle="New Updates")
 
---
+++ application/tpo.py
@@ -0,0 +1,11 @@
+tpo_data =[
+    ["2022/02/10","www.etcampusstars.com","*ET Campus Stars (ETCS) 5.0 || The Economic Times (Digital)*\nETCS 5.0 consists of four phases\nTo participate, you have to register on  www.etcampusstars.com and complete the Phase 1 Assessment on or before 10 February 2022."],
+    [
+        "2022/02/14","https://infytq.onwingspan.com/","*Infoysys || HackWithInfy 2022*"
+    ],
+    [
+        "2022/02/05","https://ideafactory.nbcbearings.com/\nAfter successful registration fill the google form attached Below\nhttps://forms.gle/HE3hQRcswxhEAbwE8", "*National Engineering Industries Ltd. | Idea Factory 2022 Season 6*\nIdea Factory Season 6 is a campus connect initiative organized by National Engineering Industries Ltd. The contest aims to Inspire, Ideate, and Innovate learning among young engineering students. The NBC IdeaFactory is in the form of the contest which enables students to tackle practical Engineering problems in form of a case study related to Manufacturing, R&D, Metallurgy, Digitization etc and provide an innovative solution."
+    ],
+    [
+        "2022/01/27","https://docs.google.com/forms/d/e/1FAIpQLSf6LSJJHwphiKNyHUE94gdCPZZmxOVYENRhEkZqmKng9tNrQQ/viewform","*CODENATION || OFF-CAMPUS* \nSummer Internship Internship profile: SDE Intern\n All the registered students will receive the test link by 30th January 2021 12 noon"
+    ]]
